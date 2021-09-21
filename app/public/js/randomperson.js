54da52c88d18dec5e6a8b613051f97aaa317d39d
samjawor <samjawor@iu.edu>
2021-09-21 18:03:31 -0400

    Code wasnt working so I made big changes to my js to try and fix

--- app/public/js/randomperson.js
+++ app/public/js/randomperson.js
@@ -11,6 +11,14 @@ const Person = {
         .then((parsedJson) => {
             console.log(parsedJson);
             this.person = parsedJson.results[0]
+            this.nameF = this.person.name.first
+            this.nameL = this.person.name.last
+            this.nationality = this.person.nat
+            this.bday = this.person.dob.date
+            this.age = this.person.dob.date
+            this.email = this.person.dob.date
+            this.picture = this.person.picture.medium
+
         })
         .catch( err => {
             console.error(err)
