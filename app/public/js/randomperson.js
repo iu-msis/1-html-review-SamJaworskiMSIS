963204d4d8a8377b32f11db0336cd0e48e45f17a
samjawor <samjawor@iu.edu>
2021-09-21 17:03:24 -0400

    Changed name of const, added code for the picture

---
+++ app/public/js/randomperson.js
@@ -0,0 +1,21 @@
+const Person = {
+    data() {
+      return {
+        "person": {},
+        }
+    },
+    created() {
+
+        fetch('https://randomuser.me/api/')
+        .then(response => response.json())
+        .then((parsedJson) => {
+            console.log(parsedJson);
+            this.person = parsedJson.results[0]
+        })
+        .catch( err => {
+            console.error(err)
+        })
+    }
+  }
+  
+Vue.createApp(Person).mount('#offerApp');
